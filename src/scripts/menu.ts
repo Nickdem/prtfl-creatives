a919fe12f4be392f682dbd597072a13b4f09db87
Демидов Никита <yotumotopow@yandex.ru>
2023-01-08 01:36:24 +0300

    fix: исправлена навигация

--- src/scripts/menu.ts
+++ src/scripts/menu.ts
@@ -12,14 +12,12 @@ function initMenu(body: HTMLBodyElement) {
   menuBtn?.addEventListener("click", showMenu);
 
   function makeActiveLink(el: Element) {
-    // разобраться с двойным нажатием и добавить нажатие на стрелку
     const activeLink = menu.querySelector("." + activeLinkClassName);
 
     if (el.className === activeLink.className) {
       return;
     }
 
-    (el.children[0] as HTMLElement).click();
     activeLink.classList.remove(activeLinkClassName);
     el.classList.add(activeLinkClassName);
   }
@@ -30,18 +28,14 @@ function initMenu(body: HTMLBodyElement) {
   }
 
   function clickHandler(e: Event) {
-    let targetEl = e.target as Element;
+    const targetEl = e.target as Element;
 
-    if (targetEl.className.indexOf("header-navigation") >= 0) {
+    if (targetEl.tagName === "NAV") {
       hideMenu();
       return;
     }
 
     if (targetEl.tagName === "A") {
-      targetEl = targetEl.parentElement;
-    }
-
-    if (targetEl.id.indexOf("link") >= 0) {
       makeActiveLink(targetEl);
       hideMenu();
     }
